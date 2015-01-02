ef2b934780fd4dc3ec2fc433d30f7f7dbe0dbc13
theoskolnik <theodoraskolnik@gmail.com>
2015-01-02 15:03:37 -0600

    implements setup method

--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -1,13 +1,20 @@
 package com.thoughtworks.accountbalance;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AccountTests {
+    private Account myAccount;
+
+    @Before
+    public void setUp() throws Exception {
+        myAccount = new Account();
+    }
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account myAccount = new Account();
         int balance = 100;
         int newBalance = myAccount.deposit(50) + balance;
         assertThat(newBalance, is(150));
@@ -15,7 +22,6 @@ public class AccountTests {
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account myAccount = new Account();
         myAccount.deposit(100);
         int newBalance = myAccount.withdraw(50);
         assertThat(newBalance, is(50));
@@ -23,7 +29,6 @@ public class AccountTests {
 
     @Test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-        Account myAccount = new Account();
         int balance = myAccount.deposit(50);
         int newBalance = myAccount.withdraw(100);
         assertThat(balance, is(newBalance));
